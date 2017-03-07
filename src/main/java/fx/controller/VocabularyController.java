503e7e62bf24f0d110059796f7461c21a6542828
shaurun <shaurun>
2017-03-07 05:22:47 +0300

    Full topic support for vocabulary

--- src/main/java/fx/controller/VocabularyController.java
+++ src/main/java/fx/controller/VocabularyController.java
@@ -16,6 +16,7 @@ import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
@@ -29,7 +30,8 @@ public class VocabularyController implements Initializable{
     private Stage stage;
     private Subject subject = UserSession.getSubject();
     private Lesson lesson = UserSession.getLesson();
-    private Word word;
+    private Word word = new Word();
+    private Topic topicToBind;
 
     private LessonDao lessonDao = new LessonDaoImpl();
     private TopicDao topicDao = new TopicDaoImpl();
@@ -45,6 +47,7 @@ public class VocabularyController implements Initializable{
     @FXML private TextField txtTranslation;
     @FXML private Hyperlink lnkTopic;
     @FXML private Button btnAdd;
+    @FXML private FlowPane cloud;
 
 
     public void delete(ActionEvent event) {
@@ -87,6 +90,7 @@ public class VocabularyController implements Initializable{
                 btnAdd.setText("Добавить слово");
                 btnAdd.setOnAction(eventHandler);
                 update();
+                word = new Word();
             }
         });
     }
@@ -97,19 +101,46 @@ public class VocabularyController implements Initializable{
     }
 
     public void add(ActionEvent event) {
-        Word word = new Word();
         word.setLesson(lesson);
         word.setWord(txtWord.getText());
         word.setTranslation(txtTranslation.getText());
         wordDao.add(word);
         txtWord.setText("");
         txtTranslation.setText("");
+        lnkTopic.setVisible(false);
+        word = new Word();
         update();
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         update();
+        initCloud();
+    }
+
+    private void initCloud() {
+        List<Topic> topics = topicDao.listTopics(subject);
+        for (final Topic topic : topics) {
+            Hyperlink topicLink = new Hyperlink(topic.getName());
+            topicLink.setBackground(new Background(
+                    new BackgroundFill(Color.web(topic.getColor()), null, null)));
+            topicLink.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    word.setTopic(topic);
+                    lnkTopic.setVisible(true);
+                    lnkTopic.setText(topic.getName());
+                    lnkTopic.setBackground(new Background(new BackgroundFill(Color.web(topic.getColor()), null, null)));
+                    lnkTopic.setOnAction(new EventHandler<ActionEvent>() {
+                        @Override
+                        public void handle(ActionEvent event) {
+                            unbindTopic(event);
+                        }
+                    });
+                }
+            });
+            cloud.getChildren().add(topicLink);
+        }
     }
 
     protected void update() {
